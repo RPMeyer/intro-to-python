f6aa8d34169ca6a242dacd9be7c3e6dc76106c76
Ryan <haru.haru77haruko@gmail.com>
2017-07-08 11:42:10 -0700

    Chapter 3 homework files prepped with intial comments

---
+++ Intro to Python/Homework/CSC110_2 Ch03/Ch03_Ex06.py
@@ -0,0 +1,5 @@
+# Use for loops to make a turtle draw these regular polygons (regular means all sides the same lengths, all angles the same):
+# • An equilateral triangle
+# • A square
+# • A hexagon (six sides)
+# • An octagon (eight sides
---
+++ Intro to Python/Homework/CSC110_2 Ch03/Ch03_Youtube.py
@@ -0,0 +1,2 @@
+#Write a program to draw a face of a clock:
+#Include tick marks for the hour and have a 'turtle' denote each hour (12)
