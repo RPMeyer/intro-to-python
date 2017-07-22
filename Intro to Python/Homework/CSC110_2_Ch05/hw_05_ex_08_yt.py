5641742358d1e10c3210ad2dcc205a5409bd3ac8
Ryan <haru.haru77haruko@gmail.com>
2017-07-22 16:23:00 -0700

    chapter 5 youtube

--- Intro to Python/Homework/CSC110_2_Ch05/hw_05_ex_08_yt.py
+++ Intro to Python/Homework/CSC110_2_Ch05/hw_05_ex_08_yt.py
@@ -2,3 +2,17 @@
 # 200 or more is filled with red,
 # values between [100 and 200) are filled with yellow and
 # bars representing values less than 100 are filled with green
+
+def draw_bar(t, height): 2 3 4 5 6
+""" Get turtle t to draw one bar, of height. """ # Added this line
+t.begin_fill() t.left(90)
+t.forward(height)
+t.write(" "+ str(height))
+t.right(90) t.forward(40) t.right(90)
+t.forward(height) t.left(90)
+t.end_fill() t.forward(10)
+wn = turtle.Screen() wn.bgcolor("lightgreen")
+tess = turtle.Turtle() tess.color("blue", "red") tess.pensize(3)
+# Create tess and set some attributes # Added this line # Set up the window and its attributes
+xs = [48,117,200,240,160,260,220] for a in xs: draw_bar(tess, a)
+wn.mainloop()
