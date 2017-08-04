ae3c8133de7e996cc533fa9743e5556fe6d3d572
Ryan <haru.haru77haruko@gmail.com>
2017-08-04 15:49:21 -0700

    fully resizable project

--- Intro to Python/Project/testProject2.py
+++ Intro to Python/Project/testProject2.py
@@ -17,7 +17,7 @@ def createPolys(poly1,poly2,poly3,poly4,color1,color2):
     s.addcomponent(poly4, color2)
 
     wn.register_shape("myshape", s)
-    #registers the user created shape to later be used as the turtle 
+    #registers the user created shape to later be used as the turtle
     #thus allowing the stamp methods to create the pattern
 
 def stampShape(color1='blue',color2='red'):
@@ -56,16 +56,18 @@ def createPattern(magVal,color1,color2, times):
     '''creates a pattern of createSquare() as a (times)x(times) square with alternating colors.
     Squares changed in size via magVal'''
     shapeSize=61*magVal
-    bounds=shapeSize*times*times #useful in determining if turtle is out of bounds, but primarily used to get screensize
+    bounds=shapeSize*times*4 #useful in determining if turtle is out of bounds, but primarily used to get screensize, 4 because squares are 4x4 individually
 
     wn.setup(width=bounds-shapeSize, height=bounds-shapeSize, startx=None, starty=None) #sets up the windows dimensions for ease of viewing
     wn.setworldcoordinates(0, 0, wn.window_width(), wn.window_height()) #starts turtle creating pattern in bottom left corner
     testTurt.penup()
 
     squareCount=0
+    x=0
     testTurt.shapesize(magVal) #resizes the pattern with a multiplier of magVal
     while(True):
-        testTurt.goto(shapeSize*squareCount,0) #prevents turtle from creating squares on top of squares on top of squares, and moves appropriate x-distance
+        testTurt.goto(shapeSize*x*4,0) #prevents turtle from creating squares on top of squares on top of squares, and moves appropriate x-distance
+        x+=1
         for i in range(0,times,1): #used to facilitate the swapping of colors and create the appropriate number of squares
             if not i%2==0:
                 createSquare(magVal,color1,color2)
@@ -76,7 +78,6 @@ def createPattern(magVal,color1,color2, times):
         color1,color2=swapColors(color1,color2) #assigns the new color values after being swapped after the if statement is executed
         if squareCount==(times*times): #16 due to being 4 x 4 square
             break
-
 #------------------------------------
 # IMPORTANT - backbone of the pattern
 #
@@ -88,6 +89,6 @@ poly2 = ((20,0),(30,0),(20,-10),(10,-10),(10,-20),(0,-30),(0,-20),(10,-10))
 poly3 = ((30,-10),(30,0),(20,-10),(10,-10),(10,-20),(0,-30),(10,-30))
 poly4 = ((10,-30),(30,-30),(30,-10))
 
-createPattern(.75,'blue', 'red', )
+createPattern(1,'blue', 'red',4)
 
 wn.exitonclick()
